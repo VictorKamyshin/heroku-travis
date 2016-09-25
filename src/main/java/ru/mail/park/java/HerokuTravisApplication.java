b3efb7f23dceabca7333eff01808ac176f055043
Ivan Sopov <sopov.ivan@gmail.com>
2016-09-25 12:01:48 +0300

    cors config

--- src/main/java/ru/mail/park/java/HerokuTravisApplication.java
+++ src/main/java/ru/mail/park/java/HerokuTravisApplication.java
@@ -2,6 +2,10 @@ package ru.mail.park.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class HerokuTravisApplication {
@@ -9,4 +13,15 @@ public class HerokuTravisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuTravisApplication.class, args);
 	}
+	
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            	registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
